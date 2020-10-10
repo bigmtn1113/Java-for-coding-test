7de19629735cc077d9c194ae80855578a766ccb7
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-10 17:47:13 +0900

    Create 7.java

---
+++ ch05_DFS BFS/7.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+class Node {
+	private int node;
+	private int distance;
+	
+	public Node(int node, int distance) {
+		this.node = node;
+		this.distance = distance;
+	}
+
+	public void show() {
+		System.out.print("(" + node + ", " + distance + ") ");
+	}
+}
+
+public class Main {
+	
+	public static void main(String[] args) {
+		ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+		
+		for (int i = 0; i < 3; ++i)
+			graph.add(new ArrayList<Node>());
+		
+		graph.get(0).add(new Node(1, 7));
+		graph.get(0).add(new Node(2, 5));
+		graph.get(1).add(new Node(0, 7));
+		graph.get(2).add(new Node(0, 5));
+		
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < graph.get(i).size(); ++j)
+				graph.get(i).get(j).show();;
+			System.out.println();
+		}
+		
+		/*
+		for (ArrayList<Node> i : graph) {
+			for (Node j : i)
+				j.show();
+			System.out.println();
+		}
+		*/
+	}
+
+}
