a799092ceda49f99e9f5f6d93545fb5a693efb1d
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-10 17:04:35 +0900

    Create 3.java

---
+++ ch05_DFS BFS/3.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void recursiveFuction() {
+		System.out.println("재귀 함수를 호출합니다.");
+		recursiveFuction();
+	}
+	
+	public static void main(String[] args) {
+		recursiveFuction();
+	}
+
+}
