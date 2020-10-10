64ca9480a44601e23d9f85b4fd1d04c0f81a03dd
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-10 17:03:59 +0900

    Create 2.java

---
+++ ch05_DFS BFS/2.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Queue<Integer> queue = new LinkedList<>();
+		
+		queue.offer(5);
+		queue.offer(2);
+		queue.offer(3);
+		queue.offer(7);
+		queue.poll();
+		queue.offer(1);
+		queue.offer(4);
+		queue.poll();
+		
+		while(!queue.isEmpty())
+			System.out.println(queue.poll());
+	}
+
+}
