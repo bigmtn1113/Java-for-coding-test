65ee9bff5bf147bdb0b8a7d1ab508f3b736a2aac
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-10 17:15:26 +0900

    Create 5.java

---
+++ ch05_DFS BFS/5.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	public static int factorialIterative(int n) {
+		int result = 1;
+		
+		for (int i = 1; i <= n; ++i)
+			result *= i;
+		return result;
+	}
+	
+	public static int factorialRecursive(int n) {
+		if (n <= 1) return 1;
+		
+		return n * factorialRecursive(n - 1);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("반복적으로 구현: " + factorialIterative(5));
+		System.out.println("재귀적으로 구현: " + factorialRecursive(5));
+	}
+
+}
