66a42811353291930a2c63f7607c49c4a8ef742e
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-10 15:23:53 +0900

    Create 1.java

---
+++ ch05_DFS BFS/1.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		
+		stack.push(5);
+		stack.push(2);
+		stack.push(3);
+		stack.push(7);
+		stack.pop();
+		stack.push(1);
+		stack.push(4);
+		stack.pop();
+		
+		while (!stack.empty())
+			System.out.println(stack.pop());
+	}
+
+}
