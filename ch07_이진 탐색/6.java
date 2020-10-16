629110423aaaad65af5ffdf71c8fa8e592a1303a
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-16 22:09:22 +0900

    Create 6.java

---
+++ ch07_이진 탐색/6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int data[] = new int[1000001];
+		
+		for (int i = 0; i < n; ++i)
+			++data[sc.nextInt()];
+		
+		int m = sc.nextInt();
+		for (int i = 0; i < m; ++i)
+			System.out.print(data[sc.nextInt()] == 0 ? "no " : "yes "); 
+		sc.close();
+	}
+
+}
