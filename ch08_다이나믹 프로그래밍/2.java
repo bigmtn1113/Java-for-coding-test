d55edd8b608fb2e293a9f4bb7ef7ea50acf03ce8
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-20 17:20:02 +0900

    Create 2.java

---
+++ ch08_다이나믹 프로그래밍/2.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static long[] mem = new long[100];
+	
+	public static long fibonachi(int x) {
+		if (x == 1 || x == 2) return 1;
+		if (mem[x] != 0) return mem[x];
+		
+		return mem[x] = fibonachi(x - 1) + fibonachi(x - 2);
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(fibonachi(50));
+	}
+
+}
