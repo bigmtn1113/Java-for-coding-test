0ac3783efff73d4d0c694af834c31fdfd94a7a50
kva231 <46125158+kva231@users.noreply.github.com>
2020-10-20 17:31:01 +0900

    Create 4.java

---
+++ ch08_다이나믹 프로그래밍/4.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		long[] mem = new long[100];
+		
+		mem[1] = 1;
+		mem[2] = 1;
+		for (int i = 3; i <= 50; ++i)
+			mem[i] = mem[i - 1] + mem[i - 2];
+		
+		System.out.print(mem[50]);
+	}
+
+}
