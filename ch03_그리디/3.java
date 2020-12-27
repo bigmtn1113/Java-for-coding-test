c26091253f2d387caaed22c74ad90be88405efd6
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-27 16:56:30 +0900

    Update 3.java

--- ch03_그리디/3.java
+++ ch03_그리디/3.java
@@ -1,41 +1,32 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 		
 		int max = 0;
-		
 		for (int i = 0; i < n; ++i) {
-			st = new StringTokenizer(br.readLine());
-			int min = 10000;
+			int min = sc.nextInt();
 			
-			for (int j = 0; j < m; ++j) {
-				int num = Integer.parseInt(st.nextToken());
+			for (int j = 1; j < m; ++j) {
+				int temp = sc.nextInt();
 				
-				if (num < min)
-					min = num;
+				if (min > temp) {
+					min = temp;
+				}
 			}
 			
-			if (min > max)
+			if (max < min) {
 				max = min;
+			}
 		}
-		br.close();
+		sc.close();
 		
-		bw.write(String.valueOf(max));
-		bw.flush();
-		bw.close();
+		System.out.print(max);
 	}
 
 }
