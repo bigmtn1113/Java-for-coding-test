90d0f1901828abbc88e27bd9a28839bd1709ca25
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-28 15:36:48 +0900

    Update 4.java

--- ch04_구현/4.java
+++ ch04_구현/4.java
@@ -9,48 +9,58 @@ public class Main {
 		int m = sc.nextInt();
 		
 		byte[][] map = new byte[n][m];
-		boolean[][] isVisit = new boolean[n][m];
+		boolean[][] visited = new boolean[n][m];
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int d = sc.nextInt();
-		isVisit[a][b] = true;
 		
-		for (int i = 0; i < n; ++i)
-			for (int j = 0; j < m; ++j)
+		visited[a][b] = true;
+		
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
 				map[i][j] = sc.nextByte();
+			}
+		}
 		sc.close();
-		
-		int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 		
 		int cnt = 1;
 		int check = 0;
+		int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+		
 		while (true) {
-			int nextA = 0, nextB = 0;
+			int nextA = a;
+			int nextB = b;
 			
 			d = (d == 0) ? 3 : d - 1;
 			++check;
 			
-			nextA = a + directions[d][0];
-			nextB = b + directions[d][1];
+			nextA += directions[d][0];
+			nextB += directions[d][1];
 			
-			if (map[nextA][nextB] == 1 || isVisit[nextA][nextB]) {
-				if (check != 4) continue;
+			if (map[nextA][nextB] == 1 || visited[nextA][nextB]) {
+				if (check != 4) {
+					continue;
+				}
 				
 				a += directions[(d + 2) % 4][0];
 				b += directions[(d + 2) % 4][1];
 				
-				if (map[a][b] == 1) break;
+				if (map[a][b] == 1) {
+					break;
+				}
+				
 				check = 0;
 			} else {
 				a = nextA;
 				b = nextB;
-				isVisit[a][b] = true;
+				visited[a][b] = true;
 				
 				++cnt;
 				check = 0;
 			}
 		}
+		
 		System.out.println(cnt);
 	}
 
