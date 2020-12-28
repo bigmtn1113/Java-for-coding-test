30c2048e5a1800cb549eff996307f4c0fd51adf4
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-28 15:23:22 +0900

    Update 2.java

--- ch04_구현/2.java
+++ ch04_구현/2.java
@@ -9,11 +9,16 @@ public class Main {
 		sc.close();
 		
 		int cnt = 0;
-		for (int h = 0; h < n + 1; ++h)
-			for (int m = 0; m < 60; ++m)
-				for (int s = 0; s < 60; ++s)
-					if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3)
+		
+		for (int h = 0; h < n + 1; ++h) {
+			for (int m = 0; m < 60; ++m) {
+				for (int s = 0; s < 60; ++s) {
+					if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3) {
 						++cnt;
+					}
+				}
+			}
+		}
 		
 		System.out.print(cnt);
 	}
