6b033659feba02cb3095cad7c14a74e725240447
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-29 19:55:02 +0900

    Update 10.java

--- ch05_DFS BFS/10.java
+++ ch05_DFS BFS/10.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-	
-	public static int[][] graph = new int[1000][1000];
-	public static int[][] directions = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+	public static int[][] iceFrame = new int[1000][1000];
+	public static int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	public static int n, m;
 	
 	public static void dfs(int x, int y) {
-		graph[x][y] = 1;
+		iceFrame[x][y] = 1;
 		
-		for (int i = 0; i < 4; ++i) {
-			int nextX = x + directions[i][0];
-			int nextY = y + directions[i][1];
+		for (int[] direction : directions) {
+			int nextX = x + direction[0];
+			int nextY = y + direction[1];
 			
-			if (nextX < 0 || nextY < 0 || nextX > n - 1 || nextY > m - 1) continue;
-			if (graph[nextX][nextY] == 1) continue;
+			if (nextX < 0 || nextY < 0 || nextX > n - 1 || nextY > m - 1) {
+				continue;
+			}
 			
-			dfs(nextX, nextY);
+			if (iceFrame[nextX][nextY] != 1) {
+				dfs(nextX, nextY);
+			}
 		}
 	}
 	
@@ -30,18 +32,20 @@ public class Main {
 		for (int i = 0; i < n; ++i) {
 			String row = sc.nextLine();
 			
-			for (int j = 0; j < m; ++j)
-				graph[i][j] = row.charAt(j) - '0';
+			for (int j = 0; j < m; ++j) {
+				iceFrame[i][j] = row.charAt(j) - '0';
+			}
 		}
 		sc.close();
 		
 		int cnt = 0;
+		
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < m; ++j) {
-				if (graph[i][j] == 1) continue;
-				
-				dfs(i, j);
-				++cnt;
+				if (iceFrame[i][j] == 0) {
+					dfs(i, j);
+					++cnt;
+				}
 			}
 		}
 		
