121f3fce14cdc5a7be3f40a1b893e9c66f6b4513
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-29 19:43:23 +0900

    Update 4.java

--- ch05_DFS BFS/4.java
+++ ch05_DFS BFS/4.java
@@ -1,7 +1,9 @@
 public class Main {
 
 	public static void recursiveFuction(int i) {
-		if (i == 100) return;
+		if (i == 100) {
+			return;
+		}
 		
 		System.out.println(i + "번째 재귀 함수에서" + (i + 1) + "번째 재귀 함수를 호출합니다.");
 		recursiveFuction(i + 1);
