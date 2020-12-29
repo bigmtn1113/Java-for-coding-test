930d89cf8296dd2b9c7046588fe98f31dab2ecca
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-29 19:57:02 +0900

    Update 11.java

--- ch05_DFS BFS/11.java
+++ ch05_DFS BFS/11.java
@@ -2,45 +2,54 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Position {
+class Coordinate {
 	private int x;
 	private int y;
 	
-	public Position(int x, int y) {
+	public Coordinate(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	public int getX() { return x; }
-	public int getY() { return y; }
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
 }
 
 public class Main {
-	
-	public static int[][] graph = new int[200][200];
-	public static int[][] directions = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-	public static int n, m;
+	public static int[][] maze = new int[200][200];
+	public static int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	public static int n;
+	public static int m;
 	
 	public static void bfs(int x, int y) {
-		Queue<Position> queue = new LinkedList<>();
-		queue.offer(new Position(x, y));
+		Queue<Coordinate> queue = new LinkedList<>();
+		
+		queue.offer(new Coordinate(x, y));
 		
 		while (!queue.isEmpty()) {
 			int currX = queue.peek().getX();
 			int currY = queue.poll().getY();
 			
-			for (int i = 0; i < 4; ++i) {
-				int nextX = currX + directions[i][0];
-				int nextY = currY + directions[i][1];
+			for (int[] direction : directions) {
+				int nextX = currX + direction[0];
+				int nextY = currY + direction[1];
 				
-				if (nextX < 0 || nextY < 0 || nextX > n - 1 || nextY > m - 1) continue;
-				if (graph[nextX][nextY] == 0) continue;
+				if (nextX < 0 || nextY < 0 || nextX > n - 1 || nextY > m - 1) {
+					continue;
+				}
 				
-				if (graph[nextX][nextY] == 1) {
-					queue.offer(new Position(nextX, nextY));
-					graph[nextX][nextY] = graph[currX][currY] + 1;
+				if (maze[nextX][nextY] == 1) {
+					queue.offer(new Coordinate(nextX, nextY));
+					maze[nextX][nextY] = maze[currX][currY] + 1;
 					
-					if (nextX == n - 1 && nextY == m - 1) return;
+					if (nextX == n - 1 && nextY == m - 1) {
+						return;
+					}
 				}
 			}
 		}
@@ -56,13 +65,14 @@ public class Main {
 		for (int i = 0; i < n; ++i) {
 			String row = sc.nextLine();
 			
-			for (int j = 0; j < m; ++j)
-				graph[i][j] = row.charAt(j) - '0';
+			for (int j = 0; j < m; ++j) {
+				maze[i][j] = row.charAt(j) - '0';
+			}
 		}
 		sc.close();
 		
 		bfs(0, 0);
-		System.out.print(graph[n - 1][m - 1]);
+		System.out.print(maze[n - 1][m - 1]);
 	}
 
 }
