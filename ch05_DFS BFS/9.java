36bb067699f0a93caef13138141a1acb3ce8ff2c
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-29 19:48:49 +0900

    Update 9.java

--- ch05_DFS BFS/9.java
+++ ch05_DFS BFS/9.java
@@ -9,6 +9,7 @@ public class Main {
 	
 	public static void bfs(int v) {
 		Queue<Integer> queue = new LinkedList<>();
+		
 		queue.offer(v);
 		visited[v] = true;
 		
@@ -16,26 +17,45 @@ public class Main {
 			int curr = queue.poll();
 			System.out.print(curr + " ");
 			
-			for (int next : graph.get(curr))
+			for (int next : graph.get(curr)) {
 				if (!visited[next]) {
 					queue.offer(next);
 					visited[next] = true;
 				}
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
-		for (int i = 0; i < 9; ++i)
+		for (int i = 0; i < 9; ++i) {
 			graph.add(new ArrayList<>());
+		}
 		
-		graph.get(1).add(2); graph.get(1).add(3); graph.get(1).add(8);
-		graph.get(2).add(1); graph.get(2).add(7);
-		graph.get(3).add(1); graph.get(3).add(4); graph.get(3).add(5);
-		graph.get(4).add(3); graph.get(4).add(5);
-		graph.get(5).add(3); graph.get(5).add(4);
+		graph.get(1).add(2);
+		graph.get(1).add(3);
+		graph.get(1).add(8);
+		
+		graph.get(2).add(1);
+		graph.get(2).add(7);
+		
+		graph.get(3).add(1);
+		graph.get(3).add(4);
+		graph.get(3).add(5);
+		
+		graph.get(4).add(3);
+		graph.get(4).add(5);
+		
+		graph.get(5).add(3);
+		graph.get(5).add(4);
+		
 		graph.get(6).add(7);
-		graph.get(7).add(2); graph.get(7).add(6); graph.get(7).add(8);
-		graph.get(8).add(1); graph.get(8).add(7);
+		
+		graph.get(7).add(2);
+		graph.get(7).add(6);
+		graph.get(7).add(8);
+		
+		graph.get(8).add(1);
+		graph.get(8).add(7);
 		
 		bfs(1);
 	}
