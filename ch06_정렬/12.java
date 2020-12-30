60ea8424d9996260d3a73bd844e6ad216d5264bf
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-30 17:52:50 +0900

    Update 12.java

--- ch06_정렬/12.java
+++ ch06_정렬/12.java
@@ -12,27 +12,36 @@ public class Main {
 		int[] a = new int[n];
 		int[] b = new int[n];
 		
-		for (int i = 0; i < n; ++i)
+		for (int i = 0; i < n; ++i) {
 			a[i] = sc.nextInt();
-		for (int i = 0; i < n; ++i)
+		}
+		
+		for (int i = 0; i < n; ++i) {
 			b[i] = sc.nextInt();
+		}
 		sc.close();
 		
 		Arrays.sort(a);
 		Arrays.sort(b);
 		
 		int reverseI = n - 1;
+		
 		for (int i = 0; i < k; ++i) {
-			if (a[i] < b[reverseI - i]) {
-				int temp = a[i];
-				a[i] = b[reverseI - i];
-				b[reverseI - i] = temp;
-			} else break;
+			if (a[i] >= b[reverseI - i]) {
+				break;
+			}
+			
+			int temp = a[i];
+			a[i] = b[reverseI - i];
+			b[reverseI - i] = temp;
 		}
 		
 		long sum = 0;
-		for (int i : a)
+		
+		for (int i : a) {
 			sum += i;
+		}
+		
 		System.out.print(sum);
 	}
 
