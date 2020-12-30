62a344c66dfd800ff959b1c67ad9da7ee8e280cf
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-30 17:31:32 +0900

    Update 11.java

--- ch06_정렬/11.java
+++ ch06_정렬/11.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Student implements Comparable<Student>{
+class Student implements Comparable<Student> {
 	private String name;
 	private int score;
 	
@@ -10,11 +10,18 @@ class Student implements Comparable<Student>{
 		this.score = score;
 	}
 	
-	public String getName() { return name; }
-	public int getScore() { return score; }
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
 
 	@Override
-	public int compareTo(Student o) { return score - o.score; }
+	public int compareTo(Student o) {
+		return score - o.score;
+	}
 }
 
 public class Main {
@@ -23,19 +30,22 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
+		
 		Student[] student = new Student[n];
 		
 		for (int i = 0; i < n; ++i) {
-			String name =sc.next();
+			String name = sc.next();
 			int score = sc.nextInt();
+			
 			student[i] = new Student(name, score);
 		}
 		sc.close();
 		
 		Arrays.sort(student);
 		
-		for (int i = 0; i < n; ++i)
+		for (int i = 0; i < n; ++i) {
 			System.out.print(student[i].getName() + " ");
+		}
 	}
 
 }
