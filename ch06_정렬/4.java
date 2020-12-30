2bed0f72b3642c30bfbee1498a21cc074601e559
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-30 17:11:08 +0900

    Update 4.java

--- ch06_정렬/4.java
+++ ch06_정렬/4.java
@@ -3,24 +3,31 @@ public class Main {
 	public static int[] array = {5, 7, 9, 0, 3, 1, 6, 2, 4, 8};
 	
 	public static void quickSort(int begin, int end) {
-		if (begin >= end) return;
+		if (begin >= end) {
+			return;
+		}
 		
 		int pivot = array[begin];
 		int left = begin + 1;
 		int right = end;
 		
 		while (left <= right) {
-			while (left <= right && array[left] <= pivot) ++left;
-			while (right > begin && array[right] >= pivot) --right;
+			while (left <= right && array[left] <= pivot) {
+				++left;
+			}
+			
+			while (right > begin && array[right] >= pivot) {
+				--right;
+			}
 			
 			if (left > right) {
-				int temp = array[begin];
-				array[begin] = array[right];
-				array[right] = temp;
+				int temp = array[right];
+				array[right] = array[begin];
+				array[begin] = temp;
 			} else {
-				int temp = array[left];
-				array[left] = array[right];
-				array[right] = temp;
+				int temp = array[right];
+				array[right] = array[left];
+				array[left] = temp;
 			}
 		}
 		
@@ -31,8 +38,9 @@ public class Main {
 	public static void main(String[] args) {
 		quickSort(0, array.length - 1);
 		
-		for (int i : array)
-			System.out.print(i + " ");
+		for (int num : array) {
+			System.out.print(num + " ");
+		}
 	}
 
 }
