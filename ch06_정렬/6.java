bcaf08307eb6a08638322c0dd7951a64ebf17b96
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-30 17:15:16 +0900

    Update 6.java

--- ch06_정렬/6.java
+++ ch06_정렬/6.java
@@ -2,18 +2,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		int[] array = {7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2};
-		
 		int max = array[0];
-		for (int i : array)
-			max = Math.max(max, i);
 		
-		int[] cnt = new int[max + 1];
-		for (int i : array)
-			++cnt[i];
+		for (int num : array) {
+			max = Math.max(max, num);
+		}
 		
-		for (int i = 0; i <= max; ++i)
-			for (int j = 0; j < cnt[i]; ++j)
+		int[] count = new int[max + 1];
+		
+		for (int num : array) {
+			++count[num];
+		}
+		
+		for (int i = 0; i <= max; ++i) {
+			for (int j = 0; j < count[i]; ++j) {
 				System.out.print(i + " ");
+			}
+		}
 	}
 
 }
