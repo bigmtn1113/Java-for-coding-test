ac4b7e6eea87504ec4a588cd2866da9f96506c67
Taesan Kim <46125158+kva231@users.noreply.github.com>
2020-12-31 19:45:40 +0900

    Update 1.java

--- ch07_이진 탐색/1.java
+++ ch07_이진 탐색/1.java
@@ -6,16 +6,18 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("생성할 원소 개수를 입력한 다음 한 칸 띄고 찾을 문자열을 입력하세요.");
+		
 		int n = sc.nextInt();
-		String str = sc.next();
+		String inputStr = sc.next();
 		sc.nextLine();
 		
 		System.out.println("앞서 적은 원소 개수만큼 문자열을 입력하세요. 구분은 띄어쓰기 한 칸으로 합니다.");
-		String inputData[] = sc.nextLine().split(" ");
+		
+		String[] array = sc.nextLine().split(" ");
 		sc.close();
 		
 		for (int i = 0; i < n; ++i) {
-			if (inputData[i].equals(str)) {
+			if (array[i].equals(inputStr)) {
 				System.out.print(i + 1);
 				break;
 			}
