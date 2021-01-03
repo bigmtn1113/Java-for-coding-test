8a06cbb4835d04fafb72627a54b813e76a3a4c59
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-03 17:50:13 +0900

    Update 5.java

--- ch07_이진 탐색/5.java
+++ ch07_이진 탐색/5.java
@@ -2,39 +2,44 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-	public static int binarySearch(int[] arr, int target, int begin, int end) {
+	public static int[] array;
+	
+	public static boolean binarySearch(int begin, int end, int target) {
 		while (begin <= end) {
 			int mid = (begin + end) / 2;
-		
-			if (arr[mid] == target) return mid;
-			else if (arr[mid] < target) begin = mid + 1;
-			else end = mid - 1;
+			
+			if (target == array[mid]) {
+				return true;
+			} else if (target < array[mid]) {
+				end = mid - 1;
+			} else {
+				begin = mid + 1;
+			}
 		}
 		
-		return -1;
+		return false;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int data[] = new int[n];
-		for (int i = 0; i < n; ++i)
-			data[i] = sc.nextInt();
+		array = new int[n];
+		
+		for (int i = 0; i < n; ++i) {
+			array[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(array);
 		
 		int m = sc.nextInt();
-		int target[] = new int[m];
-		for (int i = 0; i < m; ++i)
-			target[i] = sc.nextInt();
-		sc.close();
-		
-		Arrays.sort(data);
 		
 		for (int i = 0; i < m; ++i) {
-			int result = binarySearch(data, target[i], 0, n - 1);
-			System.out.print(result == -1 ? "no " : "yes ");
+			boolean hasNum = binarySearch(0, n - 1, sc.nextInt());
+			
+			System.out.print((hasNum == true) ? "yes " : "no ");
 		}
+		sc.close();
 	}
 
 }
