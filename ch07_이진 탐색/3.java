60e9d5016ed11b4c090edb1fea8737c34e84dbbf
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-03 17:30:37 +0900

    Update 3.java

--- ch07_이진 탐색/3.java
+++ ch07_이진 탐색/3.java
@@ -1,14 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+	public static int[] array;
 
-	public static int binarySearch(int[] arr, int target, int begin, int end) {
+	public static int binarySearch(int begin, int end, int target) {
 		while (begin <= end) {
 			int mid = (begin + end) / 2;
-		
-			if (arr[mid] == target) return mid;
-			else if (arr[mid] < target) begin = mid + 1;
-			else end = mid - 1;
+			
+			if (target == array[mid]) {
+				return mid + 1;
+			} else if (target < array[mid]) {
+				end = mid - 1;
+			} else {
+				begin = mid + 1;
+			}
 		}
 		
 		return -1;
@@ -20,13 +26,18 @@ public class Main {
 		int n = sc.nextInt();
 		int target = sc.nextInt();
 		
-		int[] arr = new int[n];
-		for (int i = 0; i < n; ++i)
-			arr[i] = sc.nextInt();
+		array = new int[n];
+		
+		for (int i = 0; i < n; ++i) {
+			array[i] = sc.nextInt();
+		}
 		sc.close();
 		
-		int result = binarySearch(arr, target, 0, n - 1);
-		System.out.print(result == -1 ? "원소가 존재하지 않습니다." : result + 1);
+		Arrays.sort(array);
+		
+		int result = binarySearch(0, n - 1, target);
+		
+		System.out.print((result != -1) ? result : "원소가 존재하지 않습니다.");
 	}
 
 }
