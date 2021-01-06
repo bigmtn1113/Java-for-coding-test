09681d4d775beb29e23152567534e955fef2dacf
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-06 17:53:31 +0900

    Update 2.java

--- ch08_다이나믹 프로그래밍/2.java
+++ ch08_다이나믹 프로그래밍/2.java
@@ -1,10 +1,14 @@
 public class Main {
-
 	public static long[] mem = new long[100];
 	
 	public static long fibonachi(int x) {
-		if (x == 1 || x == 2) return 1;
-		if (mem[x] != 0) return mem[x];
+		if (x == 1 || x == 2) {
+			return 1;
+		}
+		
+		if (mem[x] != 0) {
+			return mem[x];
+		}
 		
 		return mem[x] = fibonachi(x - 1) + fibonachi(x - 2);
 	}
