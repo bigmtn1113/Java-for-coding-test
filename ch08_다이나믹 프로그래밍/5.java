b36af11e9ca37ed37851f04c119930393a056df6
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-06 17:56:26 +0900

    Update 5.java

--- ch08_다이나믹 프로그래밍/5.java
+++ ch08_다이나믹 프로그래밍/5.java
@@ -8,15 +8,25 @@ public class Main {
 		int x = sc.nextInt();
 		sc.close();
 		
-		int dp[] = new int[x + 1];
+		int[] dp = new int[x + 1];
+		
 		for (int i = 2; i <= x; ++i) {
 			dp[i] = dp[i - 1] + 1;
-			if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-			if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-			if (i % 5 == 0) dp[i] = Math.min(dp[i], dp[i / 5] + 1);	
+			
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+			
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+			
+			if (i % 5 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 5] + 1);
+			}
 		}
 		
-		System.out.println(dp[x]);
+		System.out.print(dp[x]);
 	}
 
 }
