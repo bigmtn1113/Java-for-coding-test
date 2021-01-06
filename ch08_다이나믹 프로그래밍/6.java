9b2043bf6417047d7eeaa88930a55e9a7d21924c
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-06 17:57:38 +0900

    Update 6.java

--- ch08_다이나믹 프로그래밍/6.java
+++ ch08_다이나믹 프로그래밍/6.java
@@ -6,18 +6,22 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int[] storages = new int[n];
 		
-		for (int i = 0; i < n; ++i)
-			storages[i] = sc.nextInt();
+		int[] storage = new int[n];
+		
+		for (int i = 0 ; i < n; ++i) {
+			storage[i] = sc.nextInt();
+		}
 		sc.close();
 		
 		int[] dp = new int[n];
 		
-		dp[0] = storages[0];
-		dp[1] = Math.max(storages[0], storages[1]);
-		for (int i = 2; i < n; ++i)
-			dp[i] = Math.max(dp[i - 1], dp[i - 2] + storages[i]);
+		dp[0] = storage[0];
+		dp[1] = Math.max(storage[0], storage[1]);
+		
+		for (int i = 2; i < n; ++i) {
+			dp[i] = Math.max(dp[i - 1], dp[i - 2] + storage[i]);
+		}
 		
 		System.out.print(dp[n - 1]);
 	}
