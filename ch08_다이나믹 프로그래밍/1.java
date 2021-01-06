e2df3e3c6a8f34ead1adcbc875a29db5537b108f
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-06 17:52:16 +0900

    Update 1.java

--- ch08_다이나믹 프로그래밍/1.java
+++ ch08_다이나믹 프로그래밍/1.java
@@ -1,7 +1,9 @@
 public class Main {
 
 	public static int fibonachi(int x) {
-		if (x == 1 || x == 2) return 1;
+		if (x == 1 || x == 2) {
+			return 1;
+		}
 		
 		return fibonachi(x - 1) + fibonachi(x - 2);
 	}
