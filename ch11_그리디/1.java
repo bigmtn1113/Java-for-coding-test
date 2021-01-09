95c0db502cddb3267e71565cd633ea5187b409ac
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-09 16:51:48 +0900

    Update 1.java

--- ch11_그리디/1.java
+++ ch11_그리디/1.java
@@ -1,39 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		int n = Integer.parseInt(br.readLine());
+		int n = sc.nextInt();
+		
 		int[] fear = new int[n];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; ++i)
-			fear[i] = Integer.parseInt(st.nextToken());
-		br.close();
+		for (int i = 0; i < n; ++i) {
+			fear[i] = sc.nextInt();
+		}
+		sc.close();
 		
-		Arrays.sort(fear);	// 오름차순 정렬
+		Arrays.sort(fear);
 		
-		int cnt = 0;	// 여행을 떠날 수 있는 그룹 수
+		int groupCount = 0;
+		
 		for (int i = n - 1; i >= 0; --i) {
-			if (n < fear[i]) break;	// 현재 남은 인원의 수보다 공포도가 큰 인원이 있으면
+			if (n < fear[i]) {
+				break;
+			}
 			
-			n -= fear[i];	// 공포도만큼 사람들을 그룹에 편성. 3이면 3명 편성
-			i -= fear[i] - 1;	// 공포도가 큰 사람들부터 그룹에 편성
-			++cnt;
+			n -= fear[i];
+			i -= fear[i] - 1;
+			
+			++groupCount;
 		}
 		
-		bw.write(String.valueOf(cnt));
-		bw.flush();
-		bw.close();
+		System.out.print(groupCount);
 	}
 
 }
