3af3e621cf28c06c313276093a35b868592d08f5
Taesan Kim <46125158+kva231@users.noreply.github.com>
2021-01-09 17:04:34 +0900

    Update 6.java

--- ch11_그리디/6.java
+++ ch11_그리디/6.java
@@ -1,46 +1,80 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Food implements Comparable<Food> {
+	private int time;
+	private int index;
+
+	public Food(int time, int index) {
+		this.time = time;
+		this.index = index;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	@Override
+	public int compareTo(Food o) {
+		return time - o.time;
+	}
+}
+
 public class Solution {
 
 	public int solution(int[] food_times, long k) {
-		long foodTimesSum = 0;
-		for (int i = 0; i < food_times.length; ++i)
-			foodTimesSum += food_times[i];
-		
-		if (foodTimesSum <= k) return -1;
-		
-		int answer = 0;
-		while (k > 0) {
-			for (int i = 0; i < food_times.length; ++i) {
-				if (food_times[i] > 0) {
-					--food_times[i];
-					--k;
-					
-					if (k == 0) {
-						answer = i;
-						break;
-					}
-				}
-			}
+		long foodTimeSum = 0;
+
+		for (int food_time : food_times) {
+			foodTimeSum += food_time;
 		}
-		
-		boolean check = false;
-		for (int i = (answer + 1) % food_times.length; i < food_times.length; ++i) {
-			if (food_times[i] > 0) {
-				answer = i;
-				check = true;
+
+		if (foodTimeSum <= k) {
+			return -1;
+		}
+
+		PriorityQueue<Food> queue = new PriorityQueue<>();
+
+		for (int i = 0; i < food_times.length; ++i) {
+			queue.offer(new Food(food_times[i], i + 1));
+		}
+
+		long prevFoodTime = 0;
+
+		while (true) {
+			int foodTime = queue.peek().getTime();
+			long oneCycle = (foodTime - prevFoodTime) * queue.size();
+
+			if (k < oneCycle) {
 				break;
 			}
+
+			k -= oneCycle;
+			prevFoodTime = foodTime;
+
+			queue.poll();
 		}
-		
-		if (!check ) {
-			for (int i = 0; i < answer; ++i) {
-				if (food_times[i] > 0) {
-					answer = i;
-					break;
-				}
+
+		ArrayList<Food> result = new ArrayList<>();
+
+		while (!queue.isEmpty()) {
+			result.add(queue.poll());
+		}
+
+		Collections.sort(result, new Comparator<Food>() {
+			@Override
+			public int compare(Food o1, Food o2) {
+				return o1.getIndex() - o2.getIndex();
 			}
-		}
-		
-		return answer + 1;
+		});
+
+		return result.get((int) (k % result.size())).getIndex();
 	}
 
 }
